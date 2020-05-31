d34eb8dcea7133bc12e1ba1afbf929a793609453
Hermit Code <cheehin90@gmail.com>
2020-05-31 13:30:26 +0800

    Add files via upload

---
+++ ApplesOrange.cs
@@ -0,0 +1,62 @@
+using System.CodeDom.Compiler;
+using System.Collections.Generic;
+using System.Collections;
+using System.ComponentModel;
+using System.Diagnostics.CodeAnalysis;
+using System.Globalization;
+using System.IO;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.Serialization;
+using System.Text.RegularExpressions;
+using System.Text;
+using System;
+
+class Solution {
+
+    // Complete the countApplesAndOranges function below.
+    static void countApplesAndOranges(int s, int t, int a, int b, int[] apples, int[] oranges) {
+        int AppleCount =0,OrangeCount=0;
+        for(int i=0;i<apples.Length;i++){
+            apples[i]=apples[i]+a;
+            if(apples[i]>=s&&apples[i]<=t){
+                AppleCount++;
+            }
+        }
+        for(int i=0;i<oranges.Length;i++){
+            oranges[i]=oranges[i]+b;
+            if(oranges[i]>=s&&oranges[i]<=t){
+                OrangeCount++;
+            }
+        }
+        Console.WriteLine(AppleCount);
+        Console.WriteLine(OrangeCount);
+    }
+
+    static void Main(string[] args) {
+        string[] st = Console.ReadLine().Split(' ');
+
+        int s = Convert.ToInt32(st[0]);
+
+        int t = Convert.ToInt32(st[1]);
+
+        string[] ab = Console.ReadLine().Split(' ');
+
+        int a = Convert.ToInt32(ab[0]);
+
+        int b = Convert.ToInt32(ab[1]);
+
+        string[] mn = Console.ReadLine().Split(' ');
+
+        int m = Convert.ToInt32(mn[0]);
+
+        int n = Convert.ToInt32(mn[1]);
+
+        int[] apples = Array.ConvertAll(Console.ReadLine().Split(' '), applesTemp => Convert.ToInt32(applesTemp))
+        ;
+
+        int[] oranges = Array.ConvertAll(Console.ReadLine().Split(' '), orangesTemp => Convert.ToInt32(orangesTemp))
+        ;
+        countApplesAndOranges(s, t, a, b, apples, oranges);
+    }
+}
